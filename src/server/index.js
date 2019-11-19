f4233a84831ea5d83831c2433ac29937e5b46547
richhorvath <richard.horvath001@gmail.com>
2019-11-19 16:16:36 -0500

    initialzied react application

---
+++ src/server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use("/", express.static(path.join(__dirname, "../../dist")));
+console.log(__dirname);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("listening on port ", port));
