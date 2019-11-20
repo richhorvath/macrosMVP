e56db102dd1766a4b0e657e3990ea93d0577d11c
richhorvath <richard.horvath001@gmail.com>
2019-11-20 10:53:50 -0500

    Added meal generator based on daily average of 2000 calories, added refresh button. Added API query for nutrition information for each recipe provided and daily totals

---
+++ src/client/Meal.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import API_KEY from "../../config.js";
+export default function Meal({ meal }) {
+  const [nutrition, setNutrition] = useState({});
+  const getNutrition = () => {
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/${meal.id}/nutritionWidget.json?apiKey=${API_KEY}`
+      )
+      .then(results => {
+        let data = results.data;
+        delete data.bad;
+        delete data.good;
+        console.log(data);
+        setNutrition(data);
+      });
+  };
+  useEffect(() => {
+    getNutrition();
+  }, [meal]);
+  return (
+    <div>
+      <h2>{meal.title}</h2>
+      <img
+        src={`https://spoonacular.com/recipeImages/${meal.id}-240x150.jpg`}
+      ></img>
+      <p>calories: {nutrition.calories}</p>
+      <p>carbs: {nutrition.carbs}</p>
+      <p>protein: {nutrition.protein}</p>
+      <p>fat: {nutrition.fat}</p>
+
+      <p>ready in {meal.readyInMinutes} minutes </p>
+      <p>{meal.servings} servings</p>
+    </div>
+  );
+}
