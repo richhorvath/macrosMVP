79f1928c82325ea124b4197d038924461b788262
richhorvath <richard.horvath001@gmail.com>
2019-11-20 20:02:16 -0500

    refactored macronutrients component state to prepare for meal generation update

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,17 @@ export default function App() {
   const [meals, setMeals] = useState([]);
   const [nutrients, setNutrients] = useState({});
   const [calories, setCalories] = useState(0);
+  const [protein, setProtein] = useState(0.3);
+  const [carbs, setCarbs] = useState(0.4);
+  const [fat, setFat] = useState(0.3);
+
+  const getMacro = (percentageOfCalories, conversionRate) => {
+    let caloriesFromNutrient = calories * percentageOfCalories;
+    let gramsOfNutrient = Math.floor(caloriesFromNutrient / conversionRate);
+    console.log("calories from nutrient", caloriesFromNutrient);
+    console.log("grams", gramsOfNutrient);
+    return gramsOfNutrient;
+  };
 
   const getMeals = (calories = 2000, timeFrame = "day") => {
     axios
@@ -27,7 +38,16 @@ export default function App() {
     <div>
       <CalorieCalculator setCalories={setCalories} getMeals={getMeals} />
       <p>Your need to eat {calories} calories to meet your goals</p>
-      <Macronutrients calories={calories} />
+      <Macronutrients
+        calories={calories}
+        getMacro={getMacro}
+        protein={protein}
+        setProtein={setProtein}
+        carbs={carbs}
+        setCarbs={setCarbs}
+        fat={fat}
+        setFat={setFat}
+      />
       <button
         onClick={() => {
           getMeals(calories);
